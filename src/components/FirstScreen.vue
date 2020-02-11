a2bda2274e0bc97cf9b8edb63a7bd0a0bcd670db
Zakirov Mansur <mz@dsans.com>
2020-02-11 21:02:43 +0300

    Header and First Screen

---
+++ src/components/FirstScreen.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-img :src="require('../assets/First Screen.png')" max-width="100%">
+    <v-container>
+      <v-row class="text-center">
+        <v-col class="mt-12 mb-2">
+          <h1 style="color: white" class="display-2 font-weight-bold mb-3">
+            Что вы ищете?
+          </h1>
+        </v-col>
+
+        <v-col class="mb-5" cols="12">
+          <v-row align="center" justify="center" dense>
+            <v-spacer />
+            <v-select class="ma-4" dark label="Квартира" dense rounded filled />
+            <v-select class="ma-4" dark label="Комнаты " dense rounded filled />
+            <v-spacer />
+          </v-row>
+          <v-row>
+            <v-spacer />
+            <v-text-field label="Район, адрес." dark dense rounded filled />
+            <v-spacer />
+          </v-row>
+        </v-col>
+
+        <v-col class="mb-5" cols="12">
+          <v-btn dark text class="headline font-weight-bold mb-3">
+            <span>Поиск</span>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify"
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
+};
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar absolute app>
+    <div class="d-flex align-center">
+      <!--      <v-img-->
+      <!--        alt="Vuetify Logo"-->
+      <!--        class="shrink mr-2"-->
+      <!--        contain-->
+      <!--        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-light.png"-->
+      <!--        transition="scale-transition"-->
+      <!--        width="40"-->
+      <!--      />-->
+    </div>
+
+    <v-spacer></v-spacer>
+    <div class="d-flex align-center">
+      <v-btn target="_blank" text>
+        <span>Клиенту</span>
+      </v-btn>
+      <v-btn target="_blank" text>
+        <span>Раотнику</span>
+      </v-btn>
+      <v-btn target="_blank" text>
+        <span>Арендодателю</span>
+      </v-btn>
+      <div class="mx-3 mt-4 align-center justify-center">
+        <v-text-field
+          dense
+          rounded
+          label="Поиск"
+          prepend-inner-icon="mdi-magnify"
+        >
+        </v-text-field>
+      </div>
+      <div style="color: #7a4999">
+        <v-btn color="#7a4999" dark>
+          <v-icon>mdi-login</v-icon>
+          <span>Вход</span>
+        </v-btn>
+      </div>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
